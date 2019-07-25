086abe1492b868e9d301ecd4a2a5b86c6dd68eae
Antoine Lagadec <hello@oakho.com>
2019-07-25 15:31:08 +0200

    Fix tabbar grouping

--- layers/oakho/funcs.el
+++ layers/oakho/funcs.el
@@ -91,14 +91,63 @@
       (call-process "afplay" nil 0 nil
                     (concat  "sounds/" (nth (random (length bleep-sounds)) bleep-sounds))))))
 
+;; (defun oakho/tabbar-buffer-groups () ;; customize to show all normal files in one group
+;;   "Return the name of the tab group names the current buffer belongs to.
+;; There are two groups: Emacs buffers (those whose name starts with '*', plus
+;; dired buffers), and the rest.  This works at least with Emacs v24.2 using
+;; tabbar.el v1.7."
+;;   (list (cond ((string-match "magit:" (buffer-name)) "user")
+;;               ((string-match "magit-process:|*magit-diff:" (buffer-name)) "emacs")
+;;               ((string-equal "*" (substring (buffer-name) 0 1)) "emacs")
+;;               ((string-equal "#" (substring (buffer-name) 0 1)) "erc")
+;;               ((eq major-mode 'dired-mode) "emacs")
+;;               (t "user"))))
+
+;; https://www.emacswiki.org/emacs/TabBarMode#toc6
 (defun oakho/tabbar-buffer-groups () ;; customize to show all normal files in one group
-  "Return the name of the tab group names the current buffer belongs to.
-There are two groups: Emacs buffers (those whose name starts with '*', plus
-dired buffers), and the rest.  This works at least with Emacs v24.2 using
-tabbar.el v1.7."
-  (list (cond ((string-match "*magit:" (buffer-name)) "user")
-              ((string-match "*magit-process:|*magit-diff:" (buffer-name)) "emacs")
-              ((string-equal "*" (substring (buffer-name) 0 1)) "emacs")
-              ((string-equal "#" (substring (buffer-name) 0 1)) "erc")
-              ((eq major-mode 'dired-mode) "emacs")
-              (t "user"))))
+  "Return the list of group names the current buffer belongs to.
+        Return a list of one element based on major mode."
+      (list
+       (cond
+        ((or (get-buffer-process (current-buffer))
+             ;; Check if the major mode derives from `comint-mode' or
+             ;; `compilation-mode'.
+             (tabbar-buffer-mode-derived-p
+              major-mode '(comint-mode compilation-mode)))
+         "Process"
+         )
+        ;; ((member (buffer-name)
+        ;;          '("*scratch*" "*Messages*" "*Help*"))
+        ;;  "Common"
+        ;;  )
+        ((string-equal "*" (substring (buffer-name) 0 1))
+         "Common"
+         )
+        ((member (buffer-name)
+                 '("xyz" "day" "m3" "abi" "for" "nws" "eng" "f_g" "tim" "tmp"))
+         "Main"
+         )
+        ((eq major-mode 'dired-mode)
+         "Dired"
+         )
+        ((memq major-mode
+               '(help-mode apropos-mode Info-mode Man-mode))
+         "Common"
+         )
+        ((memq major-mode
+               '(rmail-mode
+                 rmail-edit-mode vm-summary-mode vm-mode mail-mode
+                 mh-letter-mode mh-show-mode mh-folder-mode
+                 gnus-summary-mode message-mode gnus-group-mode
+                 gnus-article-mode score-mode gnus-browse-killed-mode))
+         "Mail"
+         )
+        (t
+         ;; Return `mode-name' if not blank, `major-mode' otherwise.
+         (if (and (stringp mode-name)
+                  ;; Take care of preserving the match-data because this
+                  ;; function is called when updating the header line.
+                  (save-match-data (string-match "[^ ]" mode-name)))
+             mode-name
+           (symbol-name major-mode))
+         ))))
